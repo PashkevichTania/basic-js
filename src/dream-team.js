511ef837c04deeee08678bd8bf1fe3b0277382bf
PashkevichTania <pashkevichtania2004@gmail.com>
2021-03-14 20:44:54 +0300

    feat: start dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+    if (typeof members === 'undefined' || members === null || members.length <= 0) {
+        return false;
+    } else {
+        let result = []
+        for (let member of members) {
+            if (typeof (member) == 'string') {
+                let arr = member.replace(/\s/g, '').toUpperCase().split('')
+                result.push(arr[0])
+            }
+        }
+        return result.sort().join('');
+    }
+}
